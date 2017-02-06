243c3f53e05b4954a0298381f3170682cd50aa6f
Brian <bcobbel@gmail.com>
2017-02-05 19:41:19 -0600

    Adds some packages and CSS structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp  = require('gulp'),
 	postcss = require('gulp-postcss'),
 	autoprefixer = require('autoprefixer'),
 	cssvars = require('postcss-simple-vars'),
-	cssnested = require('postcss-nested');
+	cssnested = require('postcss-nested'),
+	cssimport = require('postcss-import');
+
 
 gulp.task('default', function(){
 	console.log("Hooray - you created a Gulp task.");
@@ -15,7 +17,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssvars, cssnested, autoprefixer]))
+	.pipe(postcss([cssimport, cssvars, cssnested, autoprefixer]))
 	.pipe(gulp.dest('./app/temp/styles'));
 });
 
